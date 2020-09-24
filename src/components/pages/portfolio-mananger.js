359d944d6bf67894c0f2023a5ee24fbe03a07701
Zazas <bran12chembah@outlook.com>
2020-09-23 21:49:04 -0400

    finalized portfolio mananger ,implemted refs

--- src/components/pages/portfolio-mananger.js
+++ src/components/pages/portfolio-mananger.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
-import PortfolioForm from '../portfolio/portfolio-form';
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 import axios from "axios";
-
 
 export default class PortfolioMananger extends Component {
   constructor() {
@@ -10,44 +9,50 @@ export default class PortfolioMananger extends Component {
     this.state = {
       portfolioItems: [],
     };
-    this.handleSuccesfullFormSubmission=this.handleSuccesfullFormSubmission.bind(this);
-    this.handleFormSubmissionError=this.handleFormSubmissionError.bind(this);
+    this.handleSuccesfullFormSubmission = this.handleSuccesfullFormSubmission.bind(
+      this
+    );
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
   }
-  handleSuccesfullFormSubmission (props){
-    // TODO
+  handleSuccesfullFormSubmission(portfolioItem) {
+    this.setState({
+      portfolioItems: [portfolioItem].concat(this.state.portfolioItems),
+    });
   }
-  handleFormSubmissionError(error){
-      console.log('error in handle form submition ',error)
+  handleFormSubmissionError(error) {
+    console.log("error in handle form submition ", error);
   }
   getPortfolioItem() {
     axios
-      .get("https://zazas.devcamp.space/portfolio/portfolio_items",{withCredentials:true})
+      .get(
+        "https://zazas.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        { withCredentials: true }
+      )
       .then((response) => {
-          console.log('portfolio items ',response);
+        console.log("portfolio items ", response);
         this.setState({
           portfolioItems: [...response.data.portfolio_items],
         });
-      }).catch((error)=>{
-          console.log('something went wrong in portfolio-mananger ' ,error )
-      }
-     ).then(()=>{
-     })
-
+      })
+      .catch((error) => {
+        console.log("something went wrong in portfolio-mananger ", error);
+      })
+      .then(() => {});
   }
-  componentDidMount(){
-      this.getPortfolioItem();
+  componentDidMount() {
+    this.getPortfolioItem();
   }
   render() {
     return (
       <div className="portfolio-mananger-container">
         <div className="left-column">
-        <PortfolioForm 
-        handleFormSubmissionError={this.handleFormSubmissionError}
-        handleSuccesfullFormSubmission={this.handleSuccesfullFormSubmission}
-        />
+          <PortfolioForm
+            handleFormSubmissionError={this.handleFormSubmissionError}
+            handleSuccesfullFormSubmission={this.handleSuccesfullFormSubmission}
+          />
         </div>
         <div className="right-column">
-          <PortfolioSidebarList data={this.state.portfolioItems}/>
+          <PortfolioSidebarList data={this.state.portfolioItems} />
         </div>
       </div>
     );
